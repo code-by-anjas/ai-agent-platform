104810de2a636fa9e21bd044c66d490436165fc0
Anjas-code <arifanjas.code@gmail.com>
2025-06-03 14:06:29 +0700

    init commit

--- src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import config from "@/lib/configs";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-
-const sql = neon(config.env.databaseUrl);
-export const db = drizzle({
-  client: sql,
-});
--- src/lib/configs.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  env: {
-    databaseUrl: process.env.DATABASE_URL as string,
-  },
-};
-
-export default config;
